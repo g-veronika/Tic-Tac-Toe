fbf1815ee6a04877fb6068cfe544356a9cc5121d
g-veronika <g.veronika2021@gmail.com>
2021-11-18 16:03:15 +0100

    board/square components added

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,10 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+const Square = () => {
+  return (
+    <div style={{
+      backgroundColor: 'gold',
+      margin: 10,
+      padding: 20
+    }}>
+      Square
+    </div>
+  );
+};
+
+
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20
+    }}>
+      Board
+      <Square />
+    </div>
+  );
+};
+
+
 const Game = () => {
   return (
-    <div>
+    <div style={{
+      backgroundColor: 'salmon',
+      margin: 10,
+      padding: 20 
+    }}>
       Game
+      <Board />
     </div>
   ); 
 };
