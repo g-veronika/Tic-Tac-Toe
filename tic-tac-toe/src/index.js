7a69bf3e53c57e7d56e921561d9df54507d56382
g-veronika <g.veronika2021@gmail.com>
2021-11-18 18:25:03 +0100

    returning early

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -23,8 +23,16 @@ const Board = () => {
 
 
   const handleClickEvent = (i) => {
-
     const newSquares = [...squares];
+
+    const winnerDeclared = Boolean(calculateWinner(newSquares));
+    const squareFilled = Boolean(newSquares[i]);
+
+    if(winnerDeclared || squareFilled) {
+      return;
+    };
+
+
     newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
     setXIsNext(!xIsNext);
