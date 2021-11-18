25928b19fa715b7a1a6aa94755b2df31bcc342d6
g-veronika <g.veronika2021@gmail.com>
2021-11-18 15:21:40 +0100

    first commit

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Game = () => {
+  return (
+    <div>
+      Game
+    </div>
+  ); 
+};
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+)
